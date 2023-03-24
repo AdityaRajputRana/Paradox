e19b124e4916c8bb46ea4ee75d405eca1497d024
Aditya Rana <adityarajputrana2016@gmail.com>
2023-03-24 19:25:19 +0530

    a. Test Level 1 b. Test Leaderboard  1. Make Profile Fragment  2. Make Rules Fragment  3. Make Prizes Fragment  4. Join Teams  5. Play Level 2

--- app/src/main/java/com/exe/paradox/MainFragments/ProfileFragment.java
+++ app/src/main/java/com/exe/paradox/MainFragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.exe.paradox.MainFragments;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,13 +9,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.exe.paradox.LoginActivity;
 import com.exe.paradox.R;
+import com.exe.paradox.databinding.FragmentLeaderboardBinding;
+import com.exe.paradox.databinding.FragmentProfileBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 
 public class ProfileFragment extends Fragment {
+
+    FragmentProfileBinding binding;
 
 
 
@@ -26,11 +34,38 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v =  inflater.inflate(R.layout.fragment_profile, container, false);
-        v.setOnClickListener(view -> {
-            FirebaseAuth.getInstance().signOut();
-            getActivity().startActivity(new Intent(getActivity(), LoginActivity.class));
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        setListeners();
+        setPersonalDetails();
+        return binding.getRoot();
+    }
+
+    private void setPersonalDetails() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null){
+            Toast.makeText(getActivity(), "Please log in to continue!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        binding.nameTxt.setText(user.getDisplayName());
+        Picasso.get()
+                .load(user.getPhotoUrl())
+                .into(binding.profileImg);
+    }
+
+    private void setListeners() {
+        binding.logoutBtn.setOnClickListener(view ->{
+            new AlertDialog.Builder(getActivity())
+                    .setTitle("Log out")
+                    .setMessage("Are you sure you want to sign out of your account?\nYour progress will remain saved on our servers.")
+                    .setCancelable(true)
+                    .setPositiveButton("Log out", (dialog, which) -> {
+                        FirebaseAuth.getInstance().signOut();
+                        getActivity().startActivity(new Intent(getActivity(), LoginActivity.class));
+                    })
+                    .setNegativeButton("Cancel", ((dialog, which) -> dialog.dismiss()))
+                    .show();
         });
-        return v;
     }
 }
