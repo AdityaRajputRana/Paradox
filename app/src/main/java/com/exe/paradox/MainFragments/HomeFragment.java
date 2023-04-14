09ed65dc1ce31c85b6ac9919010e72c34d1fd957
Aditya Rana <adityarajputrana2016@gmail.com>
2023-04-15 01:47:22 +0530

    home view pager is fixed
    Further Changes:
    1. Realtime Updates to time left and control officer answer putting.
    
    For Next year:
    a. Offer walls for coins for hints
    1. Field officer can add answers and In-app location detecting.
    2. AR Integration for FO
    3. QR Integration for FO
    4. Puzzle solved in hybrid mode for FO
    5. Hints for CO once FO solves for puzzles
    
    Test Level 2:
     1. From submitting a correct answer by CO
     2. Updating Question for whole team
     3. Updating Leaderboard to better work with team based ranking
    
     Changes to level 1:
     1. UI Improvements
     2. Hide the background while processing
     3. Level Complete message should have its own UI.
    
     Changes to Home:
     1. ViewPager becomes blank on changing fragment

--- app/src/main/java/com/exe/paradox/MainFragments/HomeFragment.java
+++ app/src/main/java/com/exe/paradox/MainFragments/HomeFragment.java
@@ -116,11 +116,16 @@ public class HomeFragment extends Fragment {
 
         //showingBanners
 
-        viewPager.setAdapter(null);
-        BannerPageAdapter adapter = new BannerPageAdapter(getChildFragmentManager(), homeRP.getBanners(), homeRP.getLeaderboard(), getActivity());
-        viewPager.setAdapter(adapter);
-        viewPager.setOffscreenPageLimit(0);
-        viewPager.setCurrentItem(0);
+        if (homeRP.getLeaderboard() != null && homeRP.getLeaderboard().size() >= 3){
+            viewPager.setVisibility(View.GONE);
+        } else {
+            viewPager.setVisibility(View.VISIBLE);
+            viewPager.setAdapter(null);
+            BannerPageAdapter adapter = new BannerPageAdapter(getChildFragmentManager(), homeRP.getBanners(), homeRP.getLeaderboard(), getActivity());
+            viewPager.setAdapter(adapter);
+            viewPager.setOffscreenPageLimit(0);
+            viewPager.setCurrentItem(0);
+        }
 
         if (homeRP.getLevel() == 1){
             isLevel1Running = true;
